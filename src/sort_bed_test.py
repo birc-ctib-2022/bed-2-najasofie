030982d4d42957ff4080d5974f8b0d39117eed8b
NajaLange <201906591@post.au.dk>
2022-10-27 12:24:47 +0200

    help merge

--- src/sort_bed_test.py
+++ src/sort_bed_test.py
@@ -7,6 +7,6 @@ from sort_bed import (
     sort_file,
 )
 
-def test_sort():
-    for i in range(len(sort_file())):
+def test_sort(x):
+    for i in range(len(sort_file(x))):
         assert sort_file(list)[i][0] <=  sort_file(list)[i+1][0] and sort_file(list)[i][1] <= sort_file(list)[i+1][1] == True
